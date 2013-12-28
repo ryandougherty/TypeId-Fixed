b9147a362c7be3decb34c6b0560493e2d2fccd58
Ryan Dougherty <redoughe@asu.edu>
2013-12-28 10:18:42 -0700

    Added more types

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <cxxabi.h>
 #include <iostream>
+#include <vector>
 #include <typeinfo>
+
+class foo { };
 
 std::string find_name(const char* str) {
 	int status;
@@ -11,14 +14,46 @@ std::string find_name(const char* str) {
 template<class T>
 std::string typeid_new(T t) { return find_name(typeid(t).name()); }
 
+template<class T>
+void print(T str) {
+	std::cout << str << std::endl;
+}
+
 int main()
 {
 	unsigned long x(5);
+	int arr[] = {3, 4, 5};
 	
-	std::cout << typeid(x).name() << std::endl;
-	std::cout << typeid_new(x) << std::endl;
+	// int
+	print(typeid(int(5)).name());				// i
+	print(typeid_new(int(5)));					// int
 	
-	std::cout << typeid("str").name() << std::endl;
-	std::cout << typeid_new("str") << std::endl;
+	// short
+	print(typeid(short(5)).name());				// s
+	print(typeid_new(short(5)));				// short
+	
+	// unsigned long
+	print(typeid(x).name());					// m
+	print(typeid_new(x));						// unsigned long
+	
+	// const char*
+	print(typeid("str").name());				// A4_c
+	print(typeid_new("str"));					// char const*
+	
+	// std::vector
+	print(typeid(std::vector<int>()).name());	// FNSt3__16vectorIiNS_9allocatorIiEEEEvE
+	print(typeid_new(std::vector<int>()));		// std::__1::vector<int, std::__1::allocator<int> >
+	
+	// std::string
+	print(typeid(std::string()).name());		// FNSt3__112basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEEEvE
+	print(typeid_new(std::string()));			// std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char> >
+	
+	// int[]
+	print(typeid(arr).name());					// A3_i
+	print(typeid_new(arr));						// int*
+	
+	// custom empty class
+	print(typeid(foo()).name());				// F3foovE
+	print(typeid_new(foo()));					// foo
 }
 
